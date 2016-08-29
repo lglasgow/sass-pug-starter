e6781daddeab3988df4a0f2f7cdb19377fe47b6e
Lee Glasgow <leeglasgow@gmail.com>
2016-08-29 15:20:21 -0700

    [initial] - Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,62 @@
+module.exports = function(grunt) {
+
+  // Loads all the grunt tasks matching grunt-*
+  require('load-grunt-tasks')(grunt);
+
+  // configure the tasks
+  grunt.initConfig({
+
+    sass: {
+      options: {
+        sourceMap: false
+      },
+      dist: {
+        files: {
+          './public/css/styles.css': './assets/sass/*.scss'
+        }
+      }
+    },
+
+    pug: {
+      compile: {
+        options: {
+          data: {},
+          pretty: true
+        },
+        files: [{
+          expand: true,
+          cwd: 'pug',
+          src: [ '*.jade', '*.pug' ],
+          dest: 'public',
+          ext: '.html'
+        }]
+      }
+    },
+
+    watch: {
+      sass: {
+        files: [ 'assets/sass/*.scss' ],
+        tasks: [ 'sass' ],
+        options: { livereload: true }
+      },
+      pug: {
+        files: [ 'pug/*.pug' ],
+        tasks: [ 'pug' ]
+      }
+    },
+
+    connect: {
+      server: {
+        options: {
+          port: 3200,
+          base: 'public'
+        }
+      }
+    }
+  });
+
+  // define the tasks
+  grunt.registerTask( 'build', [ 'sass', 'pug' ]);
+  grunt.registerTask( 'dev', [ 'connect', 'watch' ]);
+  grunt.registerTask( 'default', [ 'build', 'dev' ]);
+};
---
+++ README.md
@@ -0,0 +1,22 @@
+Basic Sass/Pug(Jade) Template
+=======================
+
+From the command line in the folder you have your new sass/pug(jade) project in, execute this command to get all the node modules needed:
+
+```
+npm install
+```
+
+Execute this to build the initial files:
+
+```
+grunt build
+```
+
+Finally execute this to watch your files for changes and to start the server:
+
+```
+grunt watch
+```
+
+Please do not check into this respository as it is a base project and needs to be used across many projects.
